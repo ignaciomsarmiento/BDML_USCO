e0e1b0eaeeafb3bf7db870d7b33258f55e3cea54
Ignacio Sarmiento-Barbieri <ignaciomsarmiento@gmail.com>
2021-10-20 22:00:29 -0300

    updates de clases

--- Tutorials/Clase1/01_Codigos.R
+++ Tutorials/Clase1/01_Codigos.R
@@ -12,13 +12,20 @@ library("tidyverse") #data wrangling
 # Leer los datos
 #(Fuente: https://www.kaggle.com/austinreese/craigslist-carstrucks-data?select=vehicles.csv)
 # # -----------------------------------------------------------------------
-set.seed(1199)
+#set.seed(1199)
 #dta_baseR<-read.csv(here("vehicles.csv"))
 # dta_tidyverse<-read_csv(here("vehicles.csv"))
 # dta_tidyverse2<-na.omit(dta_tidyverse)
-# dta_sample<-dta_tidyverse[1:100000,]
+# dta_sample<-dta_tidyverse[1:40000,]
 #write_csv(dta_sample,here("sample_vehicles.csv"))
-dta_sample<-read_csv("sample_vehicles.csv") 
+
+dta1<-read.csv("sample_vehicles.csv") #baseR
+dta_sample<-read_csv("sample_vehicles.csv") #tidyverse
+here()
+dta_sample2<-read_csv("~/Dropbox/Teaching/2021/Surcolombiana/Tutorials/sample_vehicles.csv")
+#para llamar al Help
+?read.csv
+?read_csv
 
 
 # # -----------------------------------------------------------------------
@@ -27,48 +34,60 @@ dta_sample<-read_csv("sample_vehicles.csv")
 
 #Tidyverse vs BaseR
 ## Estas dos lineas hacen lo mismo
-str(dta_sample)
-glimpse(dta_sample)
+str(dta_sample) #BaseR
+glimpse(dta_sample) #Tidyverse
   
 
-#Filtrar
-dta_sample %>% 
-  filter(is.na(price))
 
+
+#Filtrar
+missing_sample<-dta_sample %>% filter(is.na(manufacturer))
+dta_sample<-dta_sample %>% filter(!is.na(manufacturer)) #Tidyverse a través de dplyr
+dta1<- dta1[!is.na(dta1$manufacturer),]  #baseR
+
+#borrar
+rm(dta_sample2) 
+rm(mi_nombre,x,y)
+
+
+table(dta_sample$manufacturer)
+
+ferrari<- dta_sample %>% filter(manufacturer=="ferrari")
 
 # ordenar
-dta_sample %>% 
-  arrange(year)
+ferrari<- ferrari %>% arrange(price)
+ferrari<- ferrari  %>%  arrange(desc(price))
 
-dta_sample %>% 
-  arrange(desc(year))
 
 #renombrar
-dta_sample %>%
-  rename(precio=price, modelo=model, condicion=condition) 
+dta_sample<-dta_sample %>% rename(precio=price, modelo=model, condicion=condition) 
+glimpse(dta_sample) # ver un glimpse
+colnames(dta_sample) #nombre de las columnas
 
-dta_sample %>%
-  select(precio=price, modelo=model, condicion=condition) 
-
+dta_sub<-dta_sample %>% select(precio, modelo, condicion) 
+glimpse(dta_sub)
 
 
 #Fitrar y hacer resumer
 
-dta_sample %>% filter(manufacturer=="audi") %>% group_by(region) %>% summarise(odometer_mean = mean(odometer))
-summarise(group_by(filter(sample, manufacturer=="audi"), region), odometer_mean = mean(odometer))
+dta_sample %>% filter(manufacturer=="audi") %>% group_by(region) %>% summarise(odometer_mean = mean(odometer)) #tidyverse w/pipes
+
+summarise(group_by(filter(dta_sample, manufacturer=="audi"), region), odometer_mean = mean(odometer)) #sin pipes, mas dificil de leer
 
 #Recordar: Usar espacio vertical no cuesta nada y ayuda a la legibilidad
-dta_sample %>% 
-  filter(manufacturer=="audi") %>% 
-  group_by(region) %>% 
-  summarise(odometer_mean = mean(odometer))
+dta_audi<-dta_sample %>% 
+            filter(manufacturer=="audi") %>% 
+            group_by(region) %>% 
+            summarise(odometer_mean = mean(odometer))
 
-dta_sample %>% 
-  select(precio, year) %>%
-  mutate(
-    logprecio =log(precio), ## Separate with a comma
-    comment = paste0("El log precio es:", logprecio)
-  )
+
+
+dta_sample<-dta_sample %>% 
+              select(precio, year) %>%
+              mutate(
+                logprecio =log(precio), ## Separate with a comma
+                comment = paste0("El log precio es:", logprecio)
+              )
 
 
 
@@ -77,32 +96,57 @@ dta_sample %>%
 # # -----------------------------------------------------------------------
 
 plot(year,price)
-plot(dta_sample$year,dta_sample$price)
+plot(dta_sample$year,dta_sample$precio)
 
-plot(dta_sample$year,log(dta_sample$price))
+plot(dta_sample$year,log(dta_sample$precio))
 
-mean_year<- dta_sample%>%
+#media por año
+mean_year<- dta_sample %>%
   group_by(year) %>% 
-  summarise(price = mean(price,na.rm=TRUE),
+  summarise(price = mean(precio,na.rm=TRUE),
             obs=n())
 
-plot(mean_year$year,log(mean_year$price))
+
+#que esta pasando en 1993?
+y1993<- dta_sample %>% 
+          filter(year==1993) %>% 
+          arrange(desc(precio))
+
+#Filtremos ese dato raro
+dta_sample<-dta_sample %>% filter(precio<999999)
+
+
+#media por año
+mean_year<- dta_sample %>%
+  group_by(year) %>% 
+  summarise(price = mean(precio,na.rm=TRUE),
+            obs=n())
+
+plot(mean_year$year,log(mean_year$price)) #baseR
+
 
 ggplot(data=mean_year,aes(x=year,y=log(price))) +
-  geom_line() +
-  geom_smooth(method="lm")+
-  theme_bw()
+  geom_point() +
+  geom_smooth(method="lm") +
+  theme_classic()
 
 
 
 # # -----------------------------------------------------------------------
 # Regresion Lineal --------------------------------------------------------
 # # -----------------------------------------------------------------------
+?lm #se usa para modelos del estilo de clase f(x)=\beta_0 + \beta_1 x1 + ....+\beta_p xp
 
-reg1<-lm(price~odometer+factor(region),data=dta_sample)
+
+reg1<-lm(price~year,data=mean_year)
 summary(reg1)
 
-reg2<-lm(price~odometer+factor(year)+factor(region),data=dta_sample)
+mean_year<- mean_year %>% filter(price>0)
+reg2<-lm(log(price)~year,data=mean_year)
+summary(reg2)
+
+#como usar modelos lineales a traves de lm para 
+#PREDECIR!!!!!
 
 
 
