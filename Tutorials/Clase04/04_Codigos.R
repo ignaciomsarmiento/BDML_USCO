a694bee5e72dd3f6e7574be882df964d17912445
Ignacio Sarmiento-Barbieri <ignaciomsarmiento@gmail.com>
2021-10-29 18:51:49 -0300

    l5

--- Tutorials/Clase04/04_Codigos.R
+++ Tutorials/Clase04/04_Codigos.R
@@ -7,240 +7,63 @@
 # Carga de Paquetes a utilizar
 library("here") #project location
 library("tidyverse") #for data wrangling
-library("caret") #ML
+library("ISLR") #ML
 
-#install.packages("caret",dependencies = TRUE)
+#install.packages("ISLR",dependencies = TRUE)
 
 # # -----------------------------------------------------------------------
-# Classification -------------------------------------------------------------------
+# PCA -------------------------------------------------------------------
 # # -----------------------------------------------------------------------
 set.seed(101010) #sets a seed 
 
-credit<-readRDS(here("credit_class.rds")) #leer los datos
-
-#Inspeccionar los datos
-glimpse(credit)
-summary(credit)
-prop.table(table(credit$foreign)) #porcentajes
-table(credit$purpose)
-table(credit$rent)
+data(USArrests)
+View(USArrests)
 
 
-default<-credit$Default #defino ahora va a servir después
+S<-cov(USArrests)
+eigen(USArrests)
 
-#mutacion de datos
-credit<-credit %>% mutate(Default=factor(Default,levels=c(0,1),labels=c("No","Si")),
-                          history=factor(history,levels=c("good","poor","terrible"),labels=c("buena","mala","terrible")),
-                          foreign=factor(foreign,levels=c("foreign","german"),labels=c("extranjero","aleman")),
-                          purpose=factor(purpose,levels=c("newcar","usedcar","goods/repair","edu", "biz" ),labels=c("auto_nuevo","auto_usado","bienes","educacion","negocios"))) %>% 
-                   rename(historia=history,
-                          extranjero=foreign,
-                          proposito=purpose,
-                          edad=age,
-                          cantidad=amount,
-                          duracion=duration,
-                          cuotas=installment)
-
-glimpse(credit)
-# # -----------------------------------------------------------------------
-# Estadisticas Descriptivas -----------------------------------------------
-# # -----------------------------------------------------------------------
-require("gtsummary") #buen paquete para tablas descriptivas
-tbl_summary(credit)
-tbl_summary(credit,by = Default)
-
-## plot a mosaic
-plot(Default ~ historia, data=credit, col=c(8,2), ylab="Default") ## surprise!
-## the dangers of choice-based sampling!  
+eig<-eigen(S)
+eig_values<-eig$values
+eig_values<-eig$vectors
 
 
+results<-prcomp(dtaw[-1],scale=TRUE)
+ev <- results$sdev^2
+ev
+#stats::screeplot(x)
+results
+#calculate total variance explained by each principal component
+var_explained = results$sdev^2 / sum(results$sdev^2)
+length(var_explained)
+#create scree plot
+library(ggplot2)
 
 
-# # -----------------------------------------------------------------------
-# Logit  ------------------------------------------------------------------
-# # -----------------------------------------------------------------------
+db<-tibble(x=c(1:12), var=var_explained)
+db<- db %>% arrange(x) %>% mutate(var_acum=cumsum(var_explained))
 
-# usando la funcion `glm()` que viene en R
-mylogit <- glm(Default~., data = credit, family = "binomial")
-summary(mylogit,type="text")
+ggplot(db,aes(x=x,y=var_explained)) + 
+  geom_line(size=1, col="#af8dc3") +
+  geom_point(size=2, col="#af8dc3") +
+  theme_bw() +
+  scale_x_continuous(name="Componente Principal",breaks=seq(1,length(var_explained),1))+
+  scale_y_continuous(name="Proporción de Varianza Explicada",limits=c(0,1),breaks=seq(0,1,.1))+
+  #ylim(0,1.1) +
+  theme(#axis.text=element_blank(),
+    #axis.ticks=element_blank(),
+    text = element_text(size=11)
+  )
 
-pred<-predict(mylogit,newdata = credit, type = "response")
-summary(pred)
+ggplot(db,aes(x=x,y=var_acum)) + 
+  geom_line(size=1, col="#af8dc3") +
+  geom_point(size=2, col="#af8dc3") +
+  theme_bw() +
+  scale_x_continuous(name="Componente Principal",breaks=seq(1,length(var_explained),1))+
+  scale_y_continuous(name="Proporción de Varianza Explicada Acumulada",limits=c(0,1),breaks=seq(0,1,.1))+
+  #ylim(0,1.1) +
+  theme(#axis.text=element_blank(),
+    #axis.ticks=element_blank(),
+    text = element_text(size=11)
+  )
 
-
-
-
-
-
-# Caret y ML --------------------------------------------------------------
-#80% train 20% holdoup
-index <- createDataPartition(credit$Default, p = 0.8, list = FALSE)
-train <- credit[index,]
-holdout  <- credit[-index,]
-
-
-trainControl <- trainControl(
-  method = "cv",
-  number = 5,
-  classProbs = TRUE,
-  summaryFunction = twoClassSummary,
-  savePredictions = T
-)
-?trainControl #helps para ver que hay adentro
-?twoClassSummary
-
-#logit
-mylogit_caret <- train(
-  Default ~.,
-  data = train, 
-  method = "glm", #for logit
-  trControl = trainControl,
-  family = "binomial", 
-  metric = "ROC",
-  preProcess = c("center", "scale")
-)
-mylogit_caret
-
-#Lasso
-lambda_grid <- 10^seq(-3, 0.1, length = 100) #en la practica se suele usar una grilla de 200 o 300
-lambda_grid
-
-mylogit_lasso <- train(
-  Default ~., data = train, 
-  method = "glmnet",
-  trControl = trainControl,
-  family = "binomial", 
-  metric = "ROC",
-  tuneGrid = expand.grid(alpha = 0,lambda=lambda_grid), 
-  preProcess = c("center", "scale")
-)
-mylogit_lasso
-
-
-
-credx <- model.matrix( Default ~ .^2, data=credit)[,-1]
-dim(credx)
-
-mylogit_lasso_massive <- train(
-  Default ~.^2, 
-  data = train, 
-  method = "glmnet",
-  trControl = trainControl,
-  family = "binomial", 
-  metric = "ROC",
-  tuneGrid = expand.grid(alpha = 0,lambda=lambda_grid), 
-  preProcess = c("center", "scale")
-)
-
-mylogit_lasso_massive
-
-alpha_grid<-seq(0,1,by=.1)
-alpha_grid
-
-grilla_bidimensional<-expand.grid(alpha = alpha_grid,lambda=lambda_grid)
-
-mylogit_lasso_grid <- train(
-  Default ~., 
-  data = train, 
-  method = "glmnet",
-  trControl = trainControl,
-  family = "binomial", 
-  metric = "ROC",
-  tuneGrid = expand.grid(alpha = alpha_grid,lambda=lambda_grid), 
-  preProcess = c("center", "scale")
-)
-
-mylogit_lasso_grid
-
-
-#install.packages("MLeval")
-require("MLeval")
-?require
-res <- evalm(list(mylogit_caret,
-                  mylogit_lasso,
-                  mylogit_lasso_massive,
-                  mylogit_lasso_grid),gnames=c('logit','lasso',"masive","grid"))
-
-
-
-
-
-# Arbol -------------------------------------------------------------------
-
-#?rpart
-#install.packages("rpart")
-glimpse(train)
-cp_alpha<-seq(from = 0, to = 1, length = 100)
-tree <- train(
-  Default ~duracion+cantidad+cuotas+edad, 
-  data = train, 
-  method = "rpart",
-  trControl = trainControl,
-  metric = "ROC",
-  tuneGrid = expand.grid(cp = cp_alpha)
-)
-tree
-tree$bestTune
-plot(tree$finalModel)
-
-library("rattle")
-fancyRpartPlot(tree$finalModel)
-
-tree$finalModel
-
-
-# Random Forests ----------------------------------------------------------
-
-
-forest <- train(
-  Default ~., 
-  data = train, 
-  method = "rf",
-  trControl = trainControl,
-  family = "binomial", 
-  metric = "ROC",
-  preProcess = c("center", "scale")
-)
-
-
-
-adaboost <- train(
-  Default ~., data = train, 
-  method = "adaboost",
-  trControl = trainControl,
-  family = "binomial", 
-  metric = "ROC",
-  preProcess = c("center", "scale")
-)
-
-
-res <- evalm(list(mylogit_caret,
-                  mylogit_lasso,
-                  mylogit_lasso_massive,
-                  mylogit_lasso_grid,
-                  tree,
-                  forest,
-                  adaboost),
-             gnames=c('logit','lasso',"masive","grid","tree","forest","ada"))
-
-
-
-
-
-# Evaluar en los holdout sets ---------------------------------------------
-
-#Arbol
-db_tree_test<-predict(tree,newdata=holdout,type="prob")
-db_tree_test = data.frame(db_tree_test, holdout$Default)
-ev_tree<-evalm(db_tree_test)
-#.61
-#logit
-db_logit_test<-predict(mylogit_caret,newdata=holdout,type="prob")
-db_logit_test = data.frame(db_logit_test, holdout$Default)
-ev_logit<-evalm(db_logit_test)
-#.71
-
-db_lasso_test<-predict(mylogit_lasso,newdata=holdout,type="prob")
-db_lasso_test = data.frame(db_lasso_test, holdout$Default)
-ev_lasso<-evalm(db_lasso_test)
-#71
