43e6552f6bc336138898b3135755acebeb43d52a
Ignacio Sarmiento-Barbieri <ignaciomsarmiento@gmail.com>
2021-11-02 13:39:04 -0300

    codigos c5

--- Tutorials/Clase04/04_Codigos.R
+++ Tutorials/Clase04/04_Codigos.R
@@ -7,192 +7,240 @@
 # Carga de Paquetes a utilizar
 library("here") #project location
 library("tidyverse") #for data wrangling
-library("ISLR") #Introduction to statistical learning
-??ISLR #no funciona
-help(package="ISLR") #funciona
-#install.packages("ISLR",dependencies = TRUE)
+library("caret") #ML
+
+#install.packages("caret",dependencies = TRUE)
 
 # # -----------------------------------------------------------------------
-# PCA -------------------------------------------------------------------
+# Classification -------------------------------------------------------------------
 # # -----------------------------------------------------------------------
 set.seed(101010) #sets a seed 
-?USArrests
-data(USArrests)
-View(USArrests)
+
+credit<-readRDS(here("credit_class.rds")) #leer los datos
+
+#Inspeccionar los datos
+glimpse(credit)
+summary(credit)
+prop.table(table(credit$foreign)) #porcentajes
+table(credit$purpose)
+table(credit$rent)
 
 
+default<-credit$Default #defino ahora va a servir después
 
-# PCA componentes principales ---------------------------------------------
-#dSd = lambda
-# S=Cov(X)
+#mutacion de datos
+credit<-credit %>% mutate(Default=factor(Default,levels=c(0,1),labels=c("No","Si")),
+                          history=factor(history,levels=c("good","poor","terrible"),labels=c("buena","mala","terrible")),
+                          foreign=factor(foreign,levels=c("foreign","german"),labels=c("extranjero","aleman")),
+                          purpose=factor(purpose,levels=c("newcar","usedcar","goods/repair","edu", "biz" ),labels=c("auto_nuevo","auto_usado","bienes","educacion","negocios"))) %>% 
+                   rename(historia=history,
+                          extranjero=foreign,
+                          proposito=purpose,
+                          edad=age,
+                          cantidad=amount,
+                          duracion=duration,
+                          cuotas=installment)
 
-?scale #para escalar las variables
-summary(USArrests)
+glimpse(credit)
+# # -----------------------------------------------------------------------
+# Estadisticas Descriptivas -----------------------------------------------
+# # -----------------------------------------------------------------------
+require("gtsummary") #buen paquete para tablas descriptivas
+tbl_summary(credit)
+tbl_summary(credit,by = Default)
 
-?apply
-apply(USArrests,2,var) #itera sobre la dimension 2 y calcula la varianza
-diag(var(USArrests)) #calcula la matriz de varianzas y con diab() le extraigo la diagonal principal
-diag(cov(USArrests)) #similar
+## plot a mosaic
+plot(Default ~ historia, data=credit, col=c(8,2), ylab="Default") ## surprise!
+## the dangers of choice-based sampling!  
 
-#re-escalados los datos
-USArrests<- USArrests %>% mutate(Murder=scale(Murder),
-                                 Rape=scale(Rape),
-                                 Assault=scale(Assault),
-                                 UrbanPop=scale(UrbanPop))
-apply(USArrests,2,mean)
-diag(cov(USArrests)) #similar
-
-#calculo la matriz de covarianza
-S<-cov(USArrests)
-dim(S)
-
-#Calculo de los autovalores y autovectores
-eigen(USArrests)  #ojo aca la matriz no es cuadrada
-dim(USArrests)
-
-
-eig<-eigen(S)
-eig_values<-eig$values
-eig_vectors<-eig$vectors
-
-#proporcion de varianza explicada
-eig$values/sum(eig$values)
-
-
-
-#De forma automatica
-?prcomp
-pca_result<-prcomp(USArrests,center=TRUE,scale=TRUE)
-pca_result
-
-eig_vectors
-
-ev <- pca_result$sdev^2 #recordemos que los eigenvalues son la varianza
-ev
-eig_values
-
-
-
-#stats::screeplot(pca_result)
-
-#calculate total variance explained by each principal component
-var_explained = pca_result$sdev^2 / sum(pca_result$sdev^2)
-length(var_explained)
-#create scree plot
-library(ggplot2)
-
-
-db<-tibble(componentes=c(1:4), var=var_explained)
-db<- db %>% arrange(componentes) %>% mutate(var_acum=cumsum(var_explained))
-
-ggplot(db,aes(x=componentes,y=var_explained)) + 
-  geom_line(size=1, col="#af8dc3") +
-  geom_point(size=2, col="#af8dc3") +
-  theme_classic() +
-  scale_x_continuous(name="Componente Principal",breaks=seq(1,length(var_explained),1))+
-  scale_y_continuous(name="Proporción de Varianza Explicada",limits=c(0,1),breaks=seq(0,1,.1))+
-  #ylim(0,1.1) +
-  theme(#axis.text=element_blank(),
-    #axis.ticks=element_blank(),
-    text = element_text(size=11)
-  )
-
-ggplot(db,aes(x=componentes,y=var_acum)) + 
-  geom_line(size=1, col="#af8dc3") +
-  geom_point(size=2, col="#af8dc3") +
-  theme_classic() +
-  scale_x_continuous(name="Componente Principal",breaks=seq(1,length(var_explained),1))+
-  scale_y_continuous(name="Proporción de Varianza Explicada Acumulada",limits=c(0,1),breaks=seq(0,1,.1))+
-  #ylim(0,1.1) +
-  theme(#axis.text=element_blank(),
-    #axis.ticks=element_blank(),
-    text = element_text(size=11)
-  )
-
-
-pca_result$center
-pca_result$scale
-
-apply(USArrests,2,mean)
-sqrt(diag(cov(USArrests)))
-
-pca_result
-
-
-biplot(pca_result, scale = 0)
-
-delta1<-as.numeric(-1*pca_result$rotation[,1])
-X<-as.matrix(USArrests)
-delta2<-as.numeric(-1*pca_result$rotation[,2])
-
-db_cities<-tibble(city=rownames(USArrests),f1=X%*%delta1, f2=X%*%delta2)
 
 
 
 # # -----------------------------------------------------------------------
-# Clusters ----------------------------------------------------------------
+# Logit  ------------------------------------------------------------------
 # # -----------------------------------------------------------------------
-set.seed(2)
 
-x<- matrix(rnorm(50*2),ncol=2)
-x[1:25,1]<-x[1:25,1]+3
-x[1:25,2]<-x[1:25,2]-4
+# usando la funcion `glm()` que viene en R
+mylogit <- glm(Default~., data = credit, family = "binomial")
+summary(mylogit,type="text")
 
-
-plot(x[,1],x[,2])
-
-#k=2
-kmeans_results<-kmeans(x,2,nstart=20)
-plot(x,col=(kmeans_results$cluster+1))
+pred<-predict(mylogit,newdata = credit, type = "response")
+summary(pred)
 
 
 
-#k=3
-set.seed(4)
-kmeans_results<-kmeans(x,3,nstart=20)
-kmeans_results$cluster
-plot(x,col=(kmeans_results$cluster+1))
 
 
 
-#Comparar los nstart
-set.seed(4)
-kmeans_results_start1<-kmeans(x,3,nstart=1)
-kmeans_results_start1$tot.withinss
-
-kmeans_results_start20<-kmeans(x,3,nstart=20)
-kmeans_results_start20$tot.withinss
-
-kmeans_results_start50<-kmeans(x,3,nstart=50)
-kmeans_results_start50$tot.withinss
+# Caret y ML --------------------------------------------------------------
+#80% train 20% holdoup
+index <- createDataPartition(credit$Default, p = 0.8, list = FALSE)
+train <- credit[index,]
+holdout  <- credit[-index,]
 
 
+trainControl <- trainControl(
+  method = "cv",
+  number = 5,
+  classProbs = TRUE,
+  summaryFunction = twoClassSummary,
+  savePredictions = T
+)
+?trainControl #helps para ver que hay adentro
+?twoClassSummary
 
-#NCI60
-data("NCI60")
-dim(NCI60$data) #n<<<<<k
+#logit
+mylogit_caret <- train(
+  Default ~.,
+  data = train, 
+  method = "glm", #for logit
+  trControl = trainControl,
+  family = "binomial", 
+  metric = "ROC",
+  preProcess = c("center", "scale")
+)
+mylogit_caret
 
-data<-NCI60$data
-labs<-NCI60$labs
-table(labs)
+#Lasso
+lambda_grid <- 10^seq(-3, 0.1, length = 100) #en la practica se suele usar una grilla de 200 o 300
+lambda_grid
 
-pr.out<-prcomp(data,scale=TRUE)
-pr.out
+mylogit_lasso <- train(
+  Default ~., data = train, 
+  method = "glmnet",
+  trControl = trainControl,
+  family = "binomial", 
+  metric = "ROC",
+  tuneGrid = expand.grid(alpha = 0,lambda=lambda_grid), 
+  preProcess = c("center", "scale")
+)
+mylogit_lasso
 
-#crea colores
-Cols <- function(vec) {
-  cols <- rainbow(length(unique(vec)))
-  return(cols[as.numeric(as.factor(vec))]) 
-}
 
-plot(pr.out$x[, 1:2], col = Cols(labs), pch = 19, xlab = "Z1", ylab = "Z2")
-plot(pr.out$x[, 1:3], col = Cols(labs), pch = 19, xlab = "Z1", ylab = "Z3")
 
-?choose
-choose(6830,2)
+credx <- model.matrix( Default ~ .^2, data=credit)[,-1]
+dim(credx)
 
-summary(pr.out)
+mylogit_lasso_massive <- train(
+  Default ~.^2, 
+  data = train, 
+  method = "glmnet",
+  trControl = trainControl,
+  family = "binomial", 
+  metric = "ROC",
+  tuneGrid = expand.grid(alpha = 0,lambda=lambda_grid), 
+  preProcess = c("center", "scale")
+)
 
-(44/6830)*100
+mylogit_lasso_massive
 
-plot(pr.out)
+alpha_grid<-seq(0,1,by=.1)
+alpha_grid
+
+grilla_bidimensional<-expand.grid(alpha = alpha_grid,lambda=lambda_grid)
+
+mylogit_lasso_grid <- train(
+  Default ~., 
+  data = train, 
+  method = "glmnet",
+  trControl = trainControl,
+  family = "binomial", 
+  metric = "ROC",
+  tuneGrid = expand.grid(alpha = alpha_grid,lambda=lambda_grid), 
+  preProcess = c("center", "scale")
+)
+
+mylogit_lasso_grid
+
+
+#install.packages("MLeval")
+require("MLeval")
+?require
+res <- evalm(list(mylogit_caret,
+                  mylogit_lasso,
+                  mylogit_lasso_massive,
+                  mylogit_lasso_grid),gnames=c('logit','lasso',"masive","grid"))
+
+
+
+
+
+# Arbol -------------------------------------------------------------------
+
+#?rpart
+#install.packages("rpart")
+glimpse(train)
+cp_alpha<-seq(from = 0, to = 1, length = 100)
+tree <- train(
+  Default ~duracion+cantidad+cuotas+edad, 
+  data = train, 
+  method = "rpart",
+  trControl = trainControl,
+  metric = "ROC",
+  tuneGrid = expand.grid(cp = cp_alpha)
+)
+tree
+tree$bestTune
+plot(tree$finalModel)
+
+library("rattle")
+fancyRpartPlot(tree$finalModel)
+
+tree$finalModel
+
+
+# Random Forests ----------------------------------------------------------
+
+
+forest <- train(
+  Default ~., 
+  data = train, 
+  method = "rf",
+  trControl = trainControl,
+  family = "binomial", 
+  metric = "ROC",
+  preProcess = c("center", "scale")
+)
+
+
+
+adaboost <- train(
+  Default ~., data = train, 
+  method = "adaboost",
+  trControl = trainControl,
+  family = "binomial", 
+  metric = "ROC",
+  preProcess = c("center", "scale")
+)
+
+
+res <- evalm(list(mylogit_caret,
+                  mylogit_lasso,
+                  mylogit_lasso_massive,
+                  mylogit_lasso_grid,
+                  tree,
+                  forest,
+                  adaboost),
+             gnames=c('logit','lasso',"masive","grid","tree","forest","ada"))
+
+
+
+
+
+# Evaluar en los holdout sets ---------------------------------------------
+
+#Arbol
+db_tree_test<-predict(tree,newdata=holdout,type="prob")
+db_tree_test = data.frame(db_tree_test, holdout$Default)
+ev_tree<-evalm(db_tree_test)
+#.61
+#logit
+db_logit_test<-predict(mylogit_caret,newdata=holdout,type="prob")
+db_logit_test = data.frame(db_logit_test, holdout$Default)
+ev_logit<-evalm(db_logit_test)
+#.71
+
+db_lasso_test<-predict(mylogit_lasso,newdata=holdout,type="prob")
+db_lasso_test = data.frame(db_lasso_test, holdout$Default)
+ev_lasso<-evalm(db_lasso_test)
+#71
