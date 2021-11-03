195a5449ee6b6bca8e3353fbc729e48cc0c05b6c
Ignacio Sarmiento-Barbieri <ignaciomsarmiento@gmail.com>
2021-11-03 08:56:33 -0300

    init commit c6

---
+++ Tutorials/Clase06/06_Codigos.R
@@ -0,0 +1,10 @@
+#######################################################################
+#  Aprendizaje y Minería de Datos para los Negocios 
+#  Author: Ignacio Sarmiento-Barbieri (i.sarmiento at uniandes.edu.co)
+#  please do not cite or circulate without permission
+#######################################################################
+
+# Carga de Paquetes a utilizar
+library("here") #project location
+library("tidyverse") #for data wrangling
+
